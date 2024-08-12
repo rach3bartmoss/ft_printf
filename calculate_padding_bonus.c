9a0bf2f2583aacda4504a34f3db30e6fc05a7b57
Douglas Alves <157995188+rach3bartmoss@users.noreply.github.com>
2024-08-12 22:11:04 +0100

    Add files via upload
    Some upgrades to handle multiples flags that modifies the padding configuration

--- calculate_padding_bonus.c
+++ calculate_padding_bonus.c
@@ -6,13 +6,13 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 16:38:50 by dopereir          #+#    #+#             */
-/*   Updated: 2024/08/08 17:36:36 by dopereir         ###   ########.fr       */
+/*   Updated: 2024/08/12 21:38:58 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	calculate_padding_bonus(t_flags *flags, int len, int sign)
+int	calculate_padding_bonus(int value, t_flags *flags, int len, int sign)
 {
 	int	total_len;
 
@@ -22,7 +22,12 @@ int	calculate_padding_bonus(t_flags *flags, int len, int sign)
 	if (sign || flags->padding == ' ')
 		total_len++;
 	if (flags->width > total_len)
-		return (flags->width - total_len);
+	{
+		if (flags->precision == 0 || value == INT_MIN)
+			return (flags->width - total_len + 1);
+		else
+			return (flags->width - total_len);
+	}
 	else
 		return (0);
 }
